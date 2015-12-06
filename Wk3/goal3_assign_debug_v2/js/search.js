dcf771ca7952c0413c5526c9d471d527254afb9a
RickyYoder <rickyayoder15@gmail.com>
2015-12-06 14:00:09 -0500

    Adding Opening Brace For Function

--- Wk3/goal3_assign_debug_v2/js/search.js
+++ Wk3/goal3_assign_debug_v2/js/search.js
@@ -31,7 +31,7 @@
 	};
 	
 	// Finds search matches
-	var search = function(query)
+	var search = function(query){
 		
 		// split the user's search query string into an array
 		var queryArray = query.join(" ");
