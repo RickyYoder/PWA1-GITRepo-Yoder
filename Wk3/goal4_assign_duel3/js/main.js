078a1a587f875e80b415dd1ddf735ef7fcf07f40
RickyYoder <rickyayoder15@gmail.com>
2015-12-06 12:01:40 -0500

    Tweaking Markup, CSS, Defining JavaScript Vars/Functions

---
+++ Wk3/goal4_assign_duel3/js/main.js
@@ -0,0 +1,23 @@
+var playerOne = document.getElementById("playerOne"), //parent container for player stats/name
+	playerOneName = playerOne.getElementsByTagName("strong")[0], //player name
+	playerOneHealthBar = playerOne.getElementsByTagName("progress")[0], //player health bar
+	playerOneHealthValue = playerOne.getElementsByTagName("span")[0], //player health value
+	playerOneDamageCounter = 50;
+
+var playerTwo = document.getElementById("playerTwo"),
+	playerTwoName = playerOne.getElementsByTagName("strong")[0],
+	playerTwoHealthBar = playerOne.getElementsByTagName("progress")[0],
+	playerTwoHealthValue = playerOne.getElementsByTagName("span")[0],
+	playerTwoDamageCounter = 50;
+
+document.getElementById("buttonred").addEventListener('click',function(){
+
+});
+
+function fight(){
+
+}
+
+function checkWinner(){
+
+}
